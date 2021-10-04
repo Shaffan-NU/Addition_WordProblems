a2a38e55e5c1ba40286d31f0b0a1f50e9dddae53
shaffanFast <k190863@nu.edu.pk>
2021-10-04 11:12:52 +0500

    Updates

---
+++ precache-manifest.6a6875a240917940f936f8b118e37003.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a26e7c3f2d271c92506",
+    "url": "/Addition_WordProblems/static/css/main.b97e0c3d.chunk.css"
+  },
+  {
+    "revision": "0a26e7c3f2d271c92506",
+    "url": "/Addition_WordProblems/static/js/main.01d16209.chunk.js"
+  },
+  {
+    "revision": "08d901e22a9e875fb33a",
+    "url": "/Addition_WordProblems/static/js/runtime~main.d77933f9.js"
+  },
+  {
+    "revision": "bb417c14a662d7c5ae2f",
+    "url": "/Addition_WordProblems/static/js/2.11635928.chunk.js"
+  },
+  {
+    "revision": "288383014bea5f3b3885",
+    "url": "/Addition_WordProblems/static/js/3.6ab0cb74.chunk.js"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/Addition_WordProblems/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/Addition_WordProblems/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Addition_WordProblems/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Addition_WordProblems/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Addition_WordProblems/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Addition_WordProblems/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Addition_WordProblems/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Addition_WordProblems/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Addition_WordProblems/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Addition_WordProblems/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Addition_WordProblems/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Addition_WordProblems/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Addition_WordProblems/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/Addition_WordProblems/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/Addition_WordProblems/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "809774698eec4daf74e692fde3a563b3",
+    "url": "/Addition_WordProblems/index.html"
+  }
+];
